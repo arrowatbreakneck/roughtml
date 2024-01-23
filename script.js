a373b44b0f1415f3949402cb239d9e7df6a32649
arrowatbreakneck <srinivasraghav03@gmail.com>
2024-01-23 10:25:46 +0530

    no changes in readme

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("givealr");
+button.addEventListener("click",()=>{
+    prompt("congrats you sucessfully applied for quiz competition,are you ready to enjoy the thrill?")
+
+})
+if(button.addEventListener("click")){
+const body = document.getElementById("itsme");
+body.addEventListener("mousemove",()=>{
+    alert("your mouse is moving too slow");
+})}
